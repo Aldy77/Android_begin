9dbfc05a610c9e31ccf87c7d9f0a1fe4f5d5a29b
Aldinever Alzate <aalzate@sitesas.co>
2016-04-26 07:51:53 -0500

    Subir cambios realizados del project clima

---
+++ app/src/main/java/com/example/aalzate/projectclima/MainActivityClima.java
@@ -0,0 +1,13 @@
+package com.example.aalzate.projectclima;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityClima extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_clima);
+    }
+}
